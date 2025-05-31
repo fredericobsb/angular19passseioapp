0702c7e9e9f4d845c1fd06b2c40668b474ac7ff5
Frederico Lopes Ramos <frederico0707@gmail.com>
2025-05-31 11:02:22 -0300

    Trabalhando com varios ambientes ok
    Trabalhando com varios ambientes ok

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Categoria } from './categoria';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
+  apiURL:string = environment.apiURL + '/categorias';
+
   constructor(private httpClient: HttpClient) { }
 
   salvar(categoria:Categoria): Observable<Categoria>{
-      return this.httpClient.post<Categoria>('http://localhost:3000/categorias', categoria);
+      return this.httpClient.post<Categoria>(this.apiURL, categoria);
   }
 
   listarTodas(): Observable<Categoria[]>{
-    return this.httpClient.get<Categoria[]>('http://localhost:3000/categorias');
+    return this.httpClient.get<Categoria[]>(this.apiURL);
   }
 }
--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Lugar } from './lugar';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
 
+  apiURL: string = environment.apiURL + '/lugares';
+
   constructor(private httpClient: HttpClient) { }
 
   salvar(lugar:Lugar): Observable<Lugar>{
-      return this.httpClient.post<Lugar>('http://localhost:3000/lugares', lugar);
+      return this.httpClient.post<Lugar>(this.apiURL, lugar);
   }
 
   listarTodos(): Observable<Lugar[]>{
-    return this.httpClient.get<Lugar[]>('http://localhost:3000/lugares');
+    return this.httpClient.get<Lugar[]>(this.apiURL);
   }
 
   filtar(nome: string, categoria: string): Observable<Lugar[]>{
@@ -29,18 +32,18 @@ export class LugarService {
     }
     
     if(!nome && categoria && categoria === '-1'){
-      return this.httpClient.get<Lugar[]>('http://localhost:3000/lugares');
+      return this.httpClient.get<Lugar[]>(this.apiURL);
     }
 
     if(nome && categoria && categoria === '-1'){
         parametros = new HttpParams();
         parametros = parametros.set('nome_like', nome);
-        return this.httpClient.get<Lugar[]>('http://localhost:3000/lugares', {
+        return this.httpClient.get<Lugar[]>(this.apiURL, {
         params: parametros
       });
     }
    
-    return this.httpClient.get<Lugar[]>('http://localhost:3000/lugares', {
+    return this.httpClient.get<Lugar[]>(this.apiURL, {
       params: parametros
     })
   }
