d0d8fd7d697d2c8c14d380d64bfaf7fb2baa2db5
Frederico Lopes Ramos <frederico0707@gmail.com>
2025-05-31 10:02:49 -0300

    Logout ok.
    Logout ok.

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LayoutProps } from './layoutprops';
 import { ActivatedRoute, Router } from '@angular/router';
 import { filter, map } from 'rxjs';
+import { AuthgoogleService } from '../../authgoogle.service';
 
 @Component({
   selector: 'app-layout',
@@ -14,7 +15,8 @@ export class LayoutComponent implements OnInit {
   props: LayoutProps = {titulo: '', subtitulo: ''};
 
   constructor(private router: Router,
-              private activatedRoute: ActivatedRoute){}
+              private activatedRoute: ActivatedRoute,
+              private authgoogleService: AuthgoogleService ){}
 
   ngOnInit(): void {
     this.router.events
@@ -30,6 +32,10 @@ export class LayoutComponent implements OnInit {
         rotaFilha = rotaFilha.firstChild;
       }
       return rotaFilha?.snapshot.data as LayoutProps;
+  }
+
+  logout(){
+    this.authgoogleService.logout();
   }
 
 }
